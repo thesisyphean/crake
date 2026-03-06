6c9983126bd36192a4a8af062b3aca68bd6c0f02
thesisyphean <thesisyphean256@gmail.com>
2026-03-06 17:00:29 +0200

    Add alpha-beta pruning version to engine's negamax method

--- src/engine.rs
+++ src/engine.rs
@@ -1,6 +1,7 @@
 use crate::{Board, moves::Move};
-use std::cmp;
+use std::cmp::{self, Ordering};
 
+#[derive(Clone)]
 pub struct Engine<B: Board> {
     board: B,
     search_depth: u8,
@@ -17,18 +18,22 @@ impl<B: Board> Engine<B> {
         }
     }
 
-    pub fn player_move(&mut self, cmove: Move) {
+    pub fn set_search_depth(&mut self, new_depth: u8) {
+        self.search_depth = new_depth;
+    }
+
+    pub fn make_move(&mut self, cmove: Move) {
         self.board.make_move(cmove);
     }
 
-    pub fn engine_move(&mut self) -> Move {
+    pub fn best_move(&mut self) -> Move {
         let moves = self.board.generate_moves();
 
         let mut best_move = moves[0];
         let mut best_score = i32::MIN;
         for cmove in moves {
             let move_data = self.board.make_move(cmove);
-            let score = -self.negamax(self.search_depth);
+            let score = -self.negamax_alpha_beta_pruning(self.search_depth, i32::MIN + 1, i32::MAX);
             self.board.unmake_move(cmove, move_data);
 
             if score > best_score {
@@ -37,11 +42,9 @@ impl<B: Board> Engine<B> {
             }
         }
 
-        self.board.make_move(best_move);
         best_move
     }
 
-    // TODO: Alpha-beta pruning
     fn negamax(&mut self, depth: u8) -> i32 {
         if depth == 0 {
             return self.board.value();
@@ -61,5 +64,53 @@ impl<B: Board> Engine<B> {
         }
 
         best_score
+    }
+
+    fn negamax_alpha_beta_pruning(&mut self, depth: u8, mut alpha: i32, beta: i32) -> i32 {
+        if depth == 0 {
+            return self.board.value();
+        }
+
+        let mut moves = self.board.generate_moves();
+        Self::order_moves(&mut moves);
+
+        // If there are no moves available, i.e. checkmate,
+        // then the minimum score is returned, but i32::MIN cannot be negated
+        // due to overflow, so 1 is added to avoid this issue
+        let mut best_score = i32::MIN + 1;
+        for cmove in moves {
+            let move_data = self.board.make_move(cmove);
+            let score = -self.negamax_alpha_beta_pruning(depth - 1, -beta, -alpha);
+            self.board.unmake_move(cmove, move_data);
+
+            if score > best_score {
+                best_score = score;
+                alpha = cmp::max(alpha, score);
+            }
+
+            if score >= beta {
+                return best_score;
+            }
+        }
+
+        best_score
+    }
+
+    // TODO: Change this to a serious ordering
+    fn order_moves(moves: &mut Vec<Move>) {
+        moves.sort_unstable_by(|ma, mb| {
+            if let Move::Standard(_, _, Some(_)) = ma {
+                if let Move::Standard(_, _, Some(_)) = mb {
+                    return Ordering::Equal;
+                }
+                return Ordering::Less;
+            }
+
+            if let Move::Standard(_, _, Some(_)) = mb {
+                return Ordering::Greater;
+            }
+
+            Ordering::Equal
+        });
     }
 }
